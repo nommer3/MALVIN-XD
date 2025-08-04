b67a72d4cfb69ca987c82a8069eb8d25267fa222
nommer3 <botdeploiement@gmail.com>
2025-08-04 21:15:38 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "malvin~8mtkALhT#EjJrSUY2_HFgMIDPEttnoBr6IuILKzFHn2uac-5zPNg",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "24107164732", // PUT OWNER NUMBER HERE
+  PREFIX: "✓" // prefix (e.g., ., /, !, *)
 };
